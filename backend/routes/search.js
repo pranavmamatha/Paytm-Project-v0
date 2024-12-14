548a03ddc2b62df1166b9571c10b39a3b1e7401b
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 19:14:56 +0530

    created a new search router

---
+++ backend/routes/search.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../db");
+
+router.get("/", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
+module.exports = router;
