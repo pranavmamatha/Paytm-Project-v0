92e5b0276202961bf4d0054b80b7f813215150bd
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 15:59:11 +0530

    added cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+//importing cors
+const cors = require("cors");
 const app = express();
+//since frontend and backend will be hosted in different routes, cors middleware shouuld be used..
+app.use(cors()); //CORS=[CROSS ORIGIN RESOURSE SHARING]
 //importing router;
 const rootRouter = require("./routes/index");
 
