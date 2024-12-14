9ebad98ebcf3f76ab0de16c19ae365303203a64d
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 16:21:22 +0530

    routing /api/v1/user/sigin and /api/v1/user/signup to their respective routes in user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const signinRouter = require("./signin");
+const signupRouter = require("./signup");
+
+router.use("/signin", signinRouter);
+router.use("/signup", signupRouter);
 
 module.exports = router;
