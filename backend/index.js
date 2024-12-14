b594987c14ed96d70ab6bffd95a219a590e78c52
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 16:02:12 +0530

    added middlware to parse JSON bodies

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const app = express();
 //since frontend and backend will be hosted in different routes, cors middleware shouuld be used..
 app.use(cors()); //CORS=[CROSS ORIGIN RESOURSE SHARING]
+//this middleware used instead of body parser, since we are using latest version of expess
+app.use(express.json());
 //importing router;
 const rootRouter = require("./routes/index");
 
