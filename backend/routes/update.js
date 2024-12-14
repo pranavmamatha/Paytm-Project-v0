b854396a0ed60b67d569aa0ffffcfa939633512e
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 19:05:18 +0530

    completed update router

--- backend/routes/update.js
+++ backend/routes/update.js
@@ -1,4 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+const authMiddleware = require("../middleware");
+
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+  await User.updateOne({ _id: req.userId }, req.body);
+
+  res.json({
+    message: "Update successfully",
+  });
+});
 
 module.exports = router;
