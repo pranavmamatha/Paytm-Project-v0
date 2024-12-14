08183c79a1f1cb3af3653ade37b786f387f99846
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 15:54:23 +0530

    importing the sub router userRouter[/routes/user.js] in  rootRouter[/routes/index.js]

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,5 +2,10 @@
 const express = require("express");
 //Creating a router
 const router = express.Router();
+//importing sub route called user
+const userRouter = require("./user");
+
+router.use("/user", userRouter);
+
 //exporting the router
 module.exports = router;
