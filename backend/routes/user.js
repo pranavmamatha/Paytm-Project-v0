01b7c4b79dec914380a16805fbb1503699f1d5bb
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 20:31:19 +0530

    added routes to userRouter

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,12 @@ const router = express.Router();
 //importin the sub routers
 const signinRouter = require("./signin");
 const signupRouter = require("./signup");
+const updateRouter = require("./update");
+const searchRouter = require("./search");
 //routing /signin and /signup to different routes
 router.use("/signin", signinRouter);
 router.use("/signup", signupRouter);
+router.use("/", updateRouter);
+router.use("/bulk", searchRouter);
 //exporting the router
 module.exports = router;
