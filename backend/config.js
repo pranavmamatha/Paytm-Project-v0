f5e930a7f606fecdedcfd38e1190caf11e025843
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 16:07:01 +0530

    added config.js file to export jwt secret key.

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //exporting jwt secrety key
+  JWT_SECRET: "my-jwt-secret",
+};
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 //importing cors
 const cors = require("cors");
 const app = express();
+//importing router;
+const rootRouter = require("./routes/index");
 //since frontend and backend will be hosted in different routes, cors middleware shouuld be used..
 app.use(cors()); //CORS=[CROSS ORIGIN RESOURSE SHARING]
 //this middleware used instead of body parser, since we are using latest version of expess
 app.use(express.json());
-//importing router;
-const rootRouter = require("./routes/index");
 
 //routing all the request from /api/v1 to router
 app.use("/api/v1", rootRouter);
