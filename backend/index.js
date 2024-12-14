e395db6e899fcc2d66a9e804f10493815a9d4e67
pranavranjithkannan <pranavranjithkannan@gmail.com>
2024-12-14 15:45:11 +0530

    routing all the request from /api/v1 to router

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const app = express();
+//importing router;
+const { router } = require("./routes/index");
+
+//routing all the request from /api/v1 to router
+app.use("/api/v1", router);
